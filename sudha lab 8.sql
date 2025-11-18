5ab21c377f398fce0c96a345be2af8dcd645c645
dhanalakshmi <dhanalakshmi20719@gmail.com>
2025-11-18 07:47:42 +0530

    Add files via upload

---
+++ sudha lab 8.sql
@@ -0,0 +1,7 @@
+
+use britanniadb;
+select * from employees;
+insert into employees (name,department,salary) values ('nikil', 'office', 15000);
+select * from employees;
+insert into employees (name,department,salary) values ('lally', 'travel', 20000);
+select * from employees;
